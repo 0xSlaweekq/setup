73a81db1f5833e45ab5e6982b60a83bce51e3f1a
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-01-19 02:21:15 +0700

    «new»

--- vpn/vpn-install.sh
+++ vpn/vpn-install.sh
@@ -33,6 +33,15 @@ sudo apt -y --fix-broken install
 sudo apt -y autoclean
 sudo apt -y autoremove --purge
 sudo apt -y install git nano resolvconf curl wireguard wireguard-tools
+echo "Installing 3proxy"
+echo '#################################################################'
+cd ~
+curl -O https://raw.githubusercontent.com/NarcoNik/setup/main/vpn/3proxy-install.sh
+curl -O https://raw.githubusercontent.com/NarcoNik/setup/main/vpn/3proxy-uninstall.sh
+chmod +x 3proxy-install.sh
+chmod +x 3proxy-uninstall.sh
+sudo ./3proxy-install.sh
+echo '#################################################################'
 echo "Installing wireguard"
 echo '#################################################################'
 cd ~
@@ -43,25 +52,26 @@ sudo ./wireguard-install.sh
 # curl -O https://raw.githubusercontent.com/NarcoNik/setup/main/vpn/wig-install.sh
 # chmod +x wig-install.sh
 # sudo ./wig-install.sh
-echo "Installing 3proxy"
-echo '#################################################################'
-cd ~
-curl -O https://raw.githubusercontent.com/NarcoNik/setup/main/vpn/3proxy-install.sh
-curl -O https://raw.githubusercontent.com/NarcoNik/setup/main/vpn/3proxy-uninstall.sh
-chmod +x 3proxy-install.sh
-chmod +x 3proxy-uninstall.sh
-sudo ./3proxy-install.sh
 sudo reboot now
+
+
+
+
+
+
+
+
+
+
+
 # ssh-keygen -f "/home/msi/.ssh/known_hosts" -R "178.128.17.181"
 
 # curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | \
 #   sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 # curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | \
 #   sudo tee /etc/apt/sources.list.d/tailscale.list
-
 # sudo apt-get update
 # sudo apt-get install tailscale
 # sudo tailscale up
 # tailscale ip -4
-
 # 2C-4D-54-E9-02-BD
