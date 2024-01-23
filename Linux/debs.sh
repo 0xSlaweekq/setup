66a7770c44622cb7f510218d8a3514b3147f8d8c
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-01-23 13:13:27 +0700

    «new»

--- Linux/debs.sh
+++ Linux/debs.sh
@@ -2,7 +2,7 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7
 # https://ubuntu.pkgs.org/23.04/ubuntu-main-amd64/network-manager_1.42.4-1ubuntu2_amd64.deb.html
 # wget http://archive.ubuntu.com/ubuntu/pool/main/n/network-manager/network-manager_1.42.4-1ubuntu2_amd64.deb
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.0/powershell_7.4.0-1.deb_amd64.deb
+wget -q https://packages.microsoft.com/config/ubuntu/23.04/packages-microsoft-prod.deb
 wget https://dl.discordapp.net/apps/linux/0.0.40/discord-0.0.40.deb
 wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
 wget https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb
@@ -14,12 +14,14 @@ wget https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb
 
 # Install packages
 sudo apt -y install \
-  ./powershell_7.4.0-1.deb_amd64.deb ./discord-0.0.40.deb \
-  ./GitHubDesktop-linux-3.1.1-linux1.deb ./anydesk_6.1.1-1_amd64.deb
+  ./GitHubDesktop-linux-3.1.1-linux1.deb ./anydesk_6.1.1-1_amd64.deb \
+  ./packages-microsoft-prod.deb
 sudo apt -y --fix-broken install
 sudo apt install -f
 sudo systemctl daemon-reload
 # Remove packages
 sudo rm -rf \
-  powershell_7.4.0-1.deb_amd64.deb discord-0.0.40.deb \
-  GitHubDesktop-linux-3.1.1-linux1.deb anydesk_6.1.1-1_amd64.deb
+  GitHubDesktop-linux-3.1.1-linux1.deb anydesk_6.1.1-1_amd64.deb \
+  packages-microsoft-prod.deb
+sudo apt -y update
+sudo apt -y install powershell
