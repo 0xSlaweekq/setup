29b1e7c2bbc6e9e32aef5c285baf10a642f69924
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-02-21 09:24:14 +0700

    1

--- Linux/nvm.sh
+++ Linux/nvm.sh
@@ -10,4 +10,5 @@ nvm use v18.12.0
 nvm alias default v18.12.0
 sudo chown "$USER":"$USER" ~/.npm -R
 sudo chown "$USER":"$USER" ~/.nvm -R
-npm i -g yarn prettier eslint nodemon serve dotenv create-react-app
+npm i -g \
+  yarn prettier eslint solhint nodemon serve dotenv create-react-app
