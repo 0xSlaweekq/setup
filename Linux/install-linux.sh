eece2694b3c84a3c8e2a313028c9ffabcd07b394
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-03-02 11:32:12 +0700

    1

--- Linux/install-linux.sh
+++ Linux/install-linux.sh
@@ -95,24 +95,24 @@ echo 'dconf load / < dconf-settings.ini'
 
 # dconf dump / > dconf-settings.ini
 
-# bash -c \
-# 'cat << EOF > ~/.config/user-dirs.dirs
-# # This file is written by xdg-user-dirs-update
-# # If you want to change or add directories, just edit the line you're
-# # interested in. All local changes will be retained on the next run.
-# # Format is XDG_xxx_DIR="$HOME/yyy", where yyy is a shell-escaped
-# # homedir-relative path, or XDG_xxx_DIR="/yyy", where /yyy is an
-# # absolute path. No other format is supported.
-# #
-# XDG_DESKTOP_DIR="/mnt/Disk_D/home/Desktop"
-# XDG_DOWNLOAD_DIR="/mnt/Disk_D/Downloads"
-# XDG_DOCUMENTS_DIR="/mnt/Disk_D/Documents"
-# XDG_TEMPLATES_DIR="/mnt/Disk_D/home/Templates"
-# XDG_PUBLICSHARE_DIR="/mnt/Disk_D/home/Transfer"
-# XDG_MUSIC_DIR="/mnt/Disk_D/home/Music"
-# XDG_PICTURES_DIR="/mnt/Disk_D/home/Pictures"
-# XDG_VIDEOS_DIR="/mnt/Disk_D/home/Videos"
-# EOF'
+bash -c \
+'cat << EOF > ~/.config/user-dirs.dirs
+# This file is written by xdg-user-dirs-update
+# If you want to change or add directories, just edit the line youre
+# interested in. All local changes will be retained on the next run.
+# Format is XDG_xxx_DIR="$HOME/yyy", where yyy is a shell-escaped
+# homedir-relative path, or XDG_xxx_DIR="/yyy", where /yyy is an
+# absolute path. No other format is supported.
+#
+XDG_DESKTOP_DIR="$HOME/Desktop"
+XDG_DOWNLOAD_DIR="/mnt/Disk_D/Downloads"
+XDG_DOCUMENTS_DIR="/mnt/Disk_D/Documents"
+XDG_TEMPLATES_DIR="/mnt/Disk_D/home/Templates"
+XDG_PUBLICSHARE_DIR="/mnt/Disk_D/home/Transfer"
+XDG_MUSIC_DIR="/mnt/Disk_D/home/Music"
+XDG_PICTURES_DIR="/mnt/Disk_D/home/Pictures"
+XDG_VIDEOS_DIR="/mnt/Disk_D/home/Videos"
+EOF'
 
 
 
