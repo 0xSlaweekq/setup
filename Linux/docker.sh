5af15d5c2322d58d9fc8dec42cf8ee598532c4da
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-05-08 12:13:10 +0700

    1

--- Linux/docker.sh
+++ Linux/docker.sh
@@ -4,25 +4,28 @@ if [[ $(which docker) && $(docker --version) && $(docker compose) ]]; then
    echo 'Docker installed, continue...'
 else
 echo 'Docker NOT installed, continue...'
+sudo apt-get install gnome-terminal
 modprobe kvm
 modprobe kvm_intel  # Intel processors
 modprobe kvm_amd    # AMD processors
 kvm-ok
 lsmod | grep kvm
 ls -al /dev/kvm
-sudo usermod -aG kvm $USER
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y autoclean
-sudo apt-get -y autoremove --purge
+sudo usermod -aG kvm $USER && sudo groupadd docker
 sudo apt-get -y install \
   ca-certificates curl gnupg apt-transport-https lsb-release gnupg \
   software-properties-common
 sudo apt-get -y clean
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo install -m 0755 -d /etc/apt/trusted.gpg.d
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
   sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
 sudo apt-key fingerprint 0EBFCD88
-sudo echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture)] \
+  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 apt-cache policy docker-ce
@@ -38,24 +41,14 @@ echo '#### Docker installed'
 echo '#################################################################'
 fi
 
-
-
-
-
-
-
-
-
-
-# $(lsb_release -cs)
-
-# echo 'alias docker-compose="docker compose"' >> ~/.bashrc
-# docker network create traefik-public
-
 # wget https://github.com/DockStation/dockstation/releases/download/v1.5.1/dockstation_1.5.1_amd64.deb
 # sudo apt-get -y install ./dockstation_1.5.1_amd64.deb
 # sudo apt-get install -f
 # sudo rm -rf ./dockstation_1.5.1_amd64.deb
+
+# $(lsb_release -cs)
+
+# echo 'alias docker-compose="docker compose"' >> ~/.bashrc
 
 # for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-desktop docker-compose docker-compose-v2 docker-compose-plugin docker-ce-rootless-extras podman-docker containerd runc docker-buildx-plugin; do sudo apt-get -y remove --purge $pkg; done
 # rm -rf ~/.docker
@@ -73,17 +66,3 @@ fi
 #   docker-init docker-proxy dockerd dockerd-rootless-setuptool.sh \
 #   dockerd-rootless.sh rootlesskit rootlesskit-docker-proxy runc vpnkit
 # cd -
-
-# install docker desktop
-# sudo apt-get -y install gnome-terminal
-# sudo install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.1-amd64.deb
-# sudo dpkg -i ./docker-desktop-4.26.1-amd64.deb
-# rm -rf ./docker-desktop-4.26.1-amd64.deb
-# gpg --generate-key
-# pass init <your_generated_gpg_id_public_key>
-# pass init 63C4EDFC4D25FF975E422917D0BD721DCBC42507
-# systemctl --user restart docker-desktop
-# systemctl --user enable docker-desktop
--- Linux/dockernew.sh
+++ Linux/dockernew.sh
@@ -4,14 +4,6 @@ if [[ $(which docker) && $(docker --version) && $(docker compose) ]]; then
    echo 'Docker installed, continue...'
 else
 echo 'Docker NOT installed, continue...'
-for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-desktop docker-compose docker-compose-v2 docker-compose-plugin docker-ce-rootless-extras podman-docker containerd runc docker-buildx-plugin; do sudo apt-get -y remove --purge $pkg; done
-sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
-rm -rf ~/.docker
-sudo rm -rf /usr/local/bin/com.docker.cli
-sudo rm -rf /var/lib/docker
-sudo rm -rf /var/lib/containerd
-
-
 sudo apt-get install gnome-terminal
 modprobe kvm
 modprobe kvm_intel  # Intel processors
@@ -34,15 +26,12 @@ sudo systemctl restart docker
 sudo systemctl enable --now docker.service docker.socket containerd.service
 sudo systemctl daemon-reload
 
-# Verify that the Docker Engine installation is successful by running the hello-world image.
-sudo docker run hello-world
-
-wget https://desktop.docker.com/linux/main/amd64/145265/docker-desktop-4.29.0-amd64.deb
-sudo apt-get -y update
-sudo apt-get -y install ./docker-desktop-4.29.0-amd64.deb
-systemctl --user start docker-desktop
-systemctl --user enable docker-desktop
-sudo rm -rf ./docker-desktop-4.29.0-amd64.deb
+# wget https://desktop.docker.com/linux/main/amd64/145265/docker-desktop-4.29.0-amd64.deb
+# sudo apt-get -y update
+# sudo apt-get -y install ./docker-desktop-4.29.0-amd64.deb
+# systemctl --user start docker-desktop
+# systemctl --user enable docker-desktop
+# sudo rm -rf ./docker-desktop-4.29.0-amd64.deb
 
 gpg --generate-key
 pass init E20DF443A91F888DEA37E4C05673AC218BE9491C
