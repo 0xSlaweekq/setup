edd5d86a5848752bc8bf7fc4ccf198ced14d3d4a
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-06-17 21:54:20 +0700

    1

--- Linux/debs.sh
+++ Linux/debs.sh
@@ -1,20 +1,14 @@
 # Download the packages file
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7
-wget https://github.com/lutris/lutris/releases/download/v0.5.17/lutris_0.5.17_all.deb
+wget https://github.com/jgraph/drawio-desktop/releases/download/v24.5.3/drawio-amd64-24.5.3.deb
 
 # wget https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb
 # wget https://github.com/lutris/lutris/releases/download/v0.5.14/lutris_0.5.14_all.deb
-# wget https://repo.steampowered.com/steam/archive/precise/steam_latest-stable.deb
-# wget https://download.virtualbox.org/virtualbox/7.0.12/virtualbox-7.0_7.0.12-159484~Ubuntu~jammy_amd64.deb
-# wget https://download.virtualbox.org/virtualbox/7.0.12/Oracle_VM_VirtualBox_Extension_Pack-7.0.12.vbox-extpack
-# wget https://download.virtualbox.org/virtualbox/7.0.12/VirtualBoxSDK-7.0.12-159484.zip
 
 # Install packages
-sudo apt-get install -y ./lutris_0.5.17_all.deb ./packages-microsoft-prod.deb
+sudo apt-get install -y ./drawio-amd64-24.5.3.deb
 sudo apt-get install --fix-broken -y
 sudo apt-get install -y -f
 sudo systemctl daemon-reload
 # Remove packages
-sudo rm -rf packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install -y powershell
+sudo rm -rf drawio-amd64-24.5.3.deb
