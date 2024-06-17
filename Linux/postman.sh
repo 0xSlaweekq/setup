4aa04b489ea233dd2b00fea57426df8b96ee034f
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-06-17 21:10:05 +0700

    1

---
+++ Linux/postman.sh
@@ -0,0 +1,8 @@
+curl -o- "https://dl-cli.pstmn.io/install/linux64.sh" | sh
+https://dl-agent.pstmn.io/download/latest/linux
+https://dl.pstmn.io/download/latest/linux_64
+
+
+curl https://gist.githubusercontent.com/SanderTheDragon/1331397932abaa1d6fbbf63baed5f043/raw/postman-deb.sh | sh
+
+wget -O - https://gist.githubusercontent.com/SanderTheDragon/1331397932abaa1d6fbbf63baed5f043/raw/postman-deb.sh | sh
