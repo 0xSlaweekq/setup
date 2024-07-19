01baa8a0282355d55ca850e41a6984a1bf62931a
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-07-19 15:28:06 +0700

    1

--- Linux/postman.sh
+++ Linux/postman.sh
@@ -11,7 +11,7 @@ echo '#################################################################'
 #Add to sources
 curl -1sLf \
   'https://packages.konghq.com/public/insomnia/setup.deb.sh' |\
-  sudo -E distro=ubuntu codename=jammy bash
+  sudo -E distro=ubuntu codename=focal bash
 #Refresh repository sources and install Insomnia
 #Update library'
 sudo apt-get update
