a4510646f4d2efb076c4906ae7eca87fef9a7700
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-08-13 18:54:00 +0700

    1

--- Linux/nvidia.sh
+++ Linux/nvidia.sh
@@ -9,9 +9,10 @@ sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt dist-upgrade
-sudo apt install software-properties-qt # for gnome software-properties-gtk
-sudo apt install -y xserver-xorg-video-all \
-  xserver-xorg-video-intel xserver-xorg-video-nvidia-560
+sudo apt install software-properties-gtk # for kde qt, for gnome gtk
+# sudo apt install -y xserver-xorg-video-all \
+#   xserver-xorg-video-intel xserver-xorg-video-nvidia-560
+
 sudo apt install -y \
   linux-headers-$(uname -r) clang gcc make acpid \
   ca-certificates dirmngr software-properties-common apt-transport-https \
@@ -19,54 +20,50 @@ sudo apt install -y \
   libx11-dev libxmu-dev libxi-dev libglu1-mesa-dev libfreeimage-dev \
   libglfw3-dev
 sudo apt-key del 7fa2af80
-sudo apt install -y nvidia-driver-560 nvidia-headless-560 nvidia-dkms-560 \
-  nvidia-settings nvidia-prime
+
+cd ~
+wget https://download.nvidia.com/XFree86/Linux-x86_64/555.52.04/NVIDIA-Linux-x86_64-555.52.04.run
+chmod 700 NVIDIA-*.run
+sudo telinit 3
+sudo ./NVIDIA-*.run
+sudo telinit 5
+systemctl restart graphical.target
+
+# sudo apt install -y nvidia-driver-560 nvidia-headless-560 nvidia-dkms-560
+sudo apt install -y nvidia-settings nvidia-prime
+sudo ubuntu-drivers install nvidia-headless-560:{i386,amd64} \
+  nvidia-dkms-560:{i386,amd64} nvidia-driver-560:{i386,amd64}
 
 sudo apt install -y \
   libvulkan1:{i386,amd64} mesa-vulkan-drivers:{i386,amd64} \
   vkbasalt libglu1-mesa-dev:{i386,amd64} freeglut3-dev mesa-common-dev \
   libopenal1 libopenal-dev libalut0 libalut-dev
 
-sudo ubuntu-drivers install nvidia-headless-560:{i386,amd64} \
-  nvidia-dkms-560:{i386,amd64} nvidia-driver-560:{i386,amd64}
-
 apt list cuda-toolkit-* | grep -v config
 sudo apt install -y cuda-drivers cuda-toolkit nvidia-gds
 /usr/local/cuda/bin/nvcc --version
 
-sudo systemctl enable nvidia-persistenced
-sudo systemctl start nvidia-persistenced
-sudo systemctl status nvidia-persistenced
-cat /proc/driver/nvidia/version
-
-sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-
 sudo prime-select on-demand # nvidia|intel|on-demand|query
 sudo nvidia-xconfig --prime
 sh -c "xrandr --setprovideroutputsource modesetting NVIDIA-0; xrandr --auto"
-sudo mv /etc/X11/xorg.conf /etc/X11/xorg.conf-external-display
+sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+
 # Update grub2 conf
 sudo update-grub2
 
 # Update initramfs
 sudo update-initramfs -u
 
+sudo systemctl enable nvidia-persistenced
+sudo systemctl start nvidia-persistenced
+sudo systemctl status nvidia-persistenced
+cat /proc/driver/nvidia/version
+
 sudo reboot
 echo '#################################################################'
 
 
-
-# cd ~
-# wget https://download.nvidia.com/XFree86/Linux-x86_64/555.52.04/NVIDIA-Linux-x86_64-555.52.04.run
-# chmod 700 NVIDIA-*.run
-# sudo telinit 3
-# dpkg --purge nvidia-*
-# sudo apt remove --purge nvidia-*
-# sudo ./NVIDIA-Linux-x86_64-555.52.04.run
-# nvidia-xconfig # optionaly
-# sudo telinit 5
-# systemctl restart graphical.target
 
 # WINEDLLOVERRIDES="dinput8=n,b" env OBS_VKCAPTURE=1 %command%
 
