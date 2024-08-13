c3ef1bef132ee179e4beebdbf34f594a1c404978
Slaweekq <plakidin.vyacheslav@mail.ru>
2024-08-13 18:59:23 +0700

    1

--- Linux/docker.sh
+++ Linux/docker.sh
@@ -7,54 +7,51 @@ echo 'Docker NOT installed, continue...'
 sudo apt install -y gnome-terminal
 modprobe kvm
 modprobe kvm_intel  # Intel processors
+modprobe kvm_amd    # AMD processors
 kvm-ok
 lsmod | grep kvm
 ls -al /dev/kvm
 sudo usermod -aG kvm $USER && sudo groupadd docker
 sudo apt clean
 
-# Add Docker's official GPG key:
-sudo apt update
-sudo install -m 0755 -d /etc/apt/trusted.gpg.d
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-  sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
-sudo chmod a+r /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
-sudo apt-key fingerprint 0EBFCD88
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install -y \
-  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+sudo apt install -y docker-buildx-plugin docker-compose-plugin
   # docker.io containerd runc docker-compose
 
 sudo gpasswd -a $USER docker
 sudo systemctl restart docker
-sudo usermod -aG docker ${USER}
-newgrp docker
+sudo usermod -aG docker $USER
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "$HOME/.docker" -R
 
+sudo systemctl restart docker
 sudo systemctl enable --now \
-  docker docker.service containerd containerd.service && \
-  sudo systemctl daemon-reload && systemctl status docker.service
-echo 'alias docker-compose="docker compose"' >> ~/.bashrc
-source ~/.bashrc
+  docker docker.service docker.socket containerd containerd.service
+sudo systemctl daemon-reload
+ systemctl status docker.service
+
+# gpg --generate-key
+# pass init E20DF443A91F888DEA37E4C05673AC218BE9491C
 echo '#### Docker installed'
 echo '#################################################################'
 fi
 
-# wget https://github.com/DockStation/dockstation/releases/download/v1.5.1/dockstation_1.5.1_amd64.deb
-# sudo apt install -y ./dockstation_1.5.1_amd64.deb
-# sudo apt install -y -f
-# sudo rm -rf ./dockstation_1.5.1_amd64.deb
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+#   sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+# sudo chmod a+r /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+# sudo apt-key fingerprint 0EBFCD88
+# # Add the repository to Apt sources:
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt update
+# apt-cache policy docker-ce
+# sudo apt install -y \
+#   docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#   # docker.io containerd runc docker-compose
 
 # $(lsb_release -cs)
-
-# echo 'alias docker-compose="docker compose"' >> ~/.bashrc
 
 # for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-desktop docker-compose docker-compose-v2 docker-compose-plugin docker-ce-rootless-extras podman-docker containerd runc docker-buildx-plugin; do sudo apt remove --purge -y $pkg; done
 # rm -rf ~/.docker
