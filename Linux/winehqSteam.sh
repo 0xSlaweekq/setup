2528b2410f26bc1d0f102a0ce132497d4ffe216e
Slaweekq <celesik@gmail.com>
2024-08-26 00:41:21 +0700

    1

--- Linux/winehqSteam.sh
+++ Linux/winehqSteam.sh
@@ -45,6 +45,7 @@ sudo rm -rf ~/steam_latest.deb
 
 # primerun %command%
 # mangohud gamemoderun __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia %command%
+# Exec=env
 
 # sudo apt remove --purge -y ^wine
 # for .msi files wine msiexec /i
