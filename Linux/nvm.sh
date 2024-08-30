957d1cff12a8f20c2a6da5101d2566e69e5d0449
Slaweekq <celesik@gmail.com>
2024-08-31 00:41:29 +0700

    1

--- Linux/nvm.sh
+++ Linux/nvm.sh
@@ -1,18 +1,14 @@
 echo 'Install NVM & nodejs & npm'
 echo '#################################################################'
 sudo apt install -y nodejs
-# installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm --version
 nvm ls-remote
-# download and install Node.js (you may need to restart the terminal)
 nvm install 20
 nvm use 20
 nvm alias default 20
-# verifies the right Node.js version is in the environment
 node -v # should print `v20.17.0`
-# verifies the right npm version is in the environment
 npm -v # should print `10.8.2`
 
 sudo chown "$USER":"$USER" ~/.npm -R
