b768db1f895b269b8d8c53936acab128701d9f74
Slaweekq <celesik@gmail.com>
2024-08-31 00:31:40 +0700

    1

--- Linux/nvm.sh
+++ Linux/nvm.sh
@@ -1,15 +1,23 @@
 echo 'Install NVM & nodejs & npm'
 echo '#################################################################'
 sudo apt install -y nodejs
+# installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm --version
 nvm ls-remote
-nvm install v20.13.1
-nvm use v20.13.1
-nvm alias default v20.13.1
+# download and install Node.js (you may need to restart the terminal)
+nvm install 20
+nvm use 20
+nvm alias default 20
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.17.0`
+# verifies the right npm version is in the environment
+npm -v # should print `10.8.2`
+
 sudo chown "$USER":"$USER" ~/.npm -R
 sudo chown "$USER":"$USER" ~/.nvm -R
+sudo chown "$USER":"$USER" /usr/local -R
 npm i -g \
   yarn prettier eslint solhint solidity-code-metrics nodemon serve \
   dotenv nx nestjs nest-cli nats solc create-react-app npm-check-updates
---
+++ Linux/sudos.sh
@@ -0,0 +1,3 @@
+mount -o remount /
+chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
+chown root:root /var/* && chmod 4755 /var/*
