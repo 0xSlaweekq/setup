8bdcfa02f2697807fede2b6063aea452db644ddf
Slaweekq <celesik@gmail.com>
2024-09-03 00:00:31 +0700

    1

--- Linux/docker.sh
+++ Linux/docker.sh
@@ -14,9 +14,27 @@ ls -al /dev/kvm
 sudo usermod -aG kvm $USER && sudo groupadd docker
 sudo apt clean
 
-curl -fsSL https://get.docker.com -o get-docker.sh | sudo sh get-docker.sh
-sudo apt install -y docker-buildx-plugin docker-compose-plugin
+# curl -fsSL https://get.docker.com -o get-docker.sh | sudo sh get-docker.sh
+# sudo apt install -y docker-buildx-plugin docker-compose-plugin
   # docker.io containerd runc docker-compose
+# Add Docker's official GPG key:
+sudo apt update
+sudo install -m 0755 -d /etc/apt/trusted.gpg.d
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+  sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+sudo chmod a+r /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+sudo apt-key fingerprint 0EBFCD88
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y \
+  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  # docker.io containerd runc docker-compose
+
 
 sudo gpasswd -a $USER docker
 sudo systemctl restart docker
@@ -32,3 +50,24 @@ sudo systemctl daemon-reload
 echo '#### Docker installed'
 echo '#################################################################'
 fi
+
+
+# $(lsb_release -cs)
+
+# for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-desktop docker-compose docker-compose-v2 docker-compose-plugin docker-ce-rootless-extras podman-docker containerd runc docker-buildx-plugin; do sudo apt remove --purge -y $pkg; done
+# rm -rf ~/.docker
+# sudo rm -rf /etc/apt/sources.list.d/docker.list
+# sudo rm -rf /usr/local/bin/com.docker.cli
+# sudo rm -rf /etc/apt/sources.list.d/archive_uri-https_download_docker_com_linux_ubuntu-lunar.list
+# sudo rm -rf /etc/apt/sources.list.d/docker.list
+# sudo rm -rf /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+# sudo rm -rf /etc/apt/keyrings/docker.gpg
+# sudo rm -rf /var/lib/docker
+# sudo rm -rf /var/lib/containerd
+# sudo rm -rf ~/.docker
+# sudo rm -rf /usr/local/bin/com.docker.cli
+# cd /bin
+# sudo rm -rf containerd containerd-shim containerd-shim-runc-v2 ctr docker \
+#   docker-init docker-proxy dockerd dockerd-rootless-setuptool.sh \
+#   dockerd-rootless.sh rootlesskit rootlesskit-docker-proxy runc vpnkit
+# cd -
