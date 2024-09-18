6c9e3d0f369aa839ec6ef05cab30f9083ce284cd
Slaweekq <celesik@gmail.com>
2024-09-18 20:17:57 +0700

    1

--- Linux/nvm.sh
+++ Linux/nvm.sh
@@ -4,6 +4,15 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install -y nodejs
+npm config set prefix ~/.npm
+export PATH="$PATH:$HOME/.npm/bin"
+tee -a ~/.bashrc <<< '
+export PATH="$PATH:$HOME/.npm/bin"'
+source ~/.bashrc
+npm i -g \
+  yarn prettier eslint solhint solidity-code-metrics nodemon serve \
+  dotenv nx nestjs nest-cli nats solc create-react-app npm-check-updates
+clear
 
 echo 'Install NVM & npm'
 echo '#################################################################'
@@ -15,9 +24,6 @@ VERSION=20.13.1
 nvm install $VERSION
 nvm use $VERSION
 nvm alias default $VERSION
-sudo chown "$USER":"$USER" ~/.npm -R
-sudo chown "$USER":"$USER" ~/.nvm -R
-npm i -g \
-  yarn prettier eslint solhint solidity-code-metrics nodemon serve \
-  dotenv nx nestjs nest-cli nats solc create-react-app npm-check-updates
+sudo chown -R "$USER":"$USER" ~/.npm
+sudo chown -R "$USER":"$USER" ~/.nvm
 clear && nvm ls
