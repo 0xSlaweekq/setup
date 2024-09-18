4996a5d0b69de276b452ac8073bdcce88d2cf3a0
Slaweekq <celesik@gmail.com>
2024-09-18 20:00:19 +0700

    1

--- Linux/nvm.sh
+++ Linux/nvm.sh
@@ -1,6 +1,12 @@
-echo 'Install NVM & nodejs & npm'
+echo 'Install nodejs & npm'
 echo '#################################################################'
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/nodesource.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
 sudo apt install -y nodejs
+
+echo 'Install NVM & npm'
+echo '#################################################################'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm --version
