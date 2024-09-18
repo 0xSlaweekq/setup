eec8a4ae26476bf121f3f4d9bd4c07f6eac948ff
Slaweekq <celesik@gmail.com>
2024-09-18 20:26:05 +0700

    1

--- Linux/nvm.sh
+++ Linux/nvm.sh
@@ -4,15 +4,11 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install -y nodejs
-npm config set prefix ~/.npm
 export PATH="$PATH:$HOME/.npm/bin"
-tee -a ~/.bashrc <<< '
-export PATH="$PATH:$HOME/.npm/bin"'
 source ~/.bashrc
-npm i -g \
+npm i -g npm@10.5.2 \
   yarn prettier eslint solhint solidity-code-metrics nodemon serve \
   dotenv nx nestjs nest-cli nats solc create-react-app npm-check-updates
-clear
 
 echo 'Install NVM & npm'
 echo '#################################################################'
