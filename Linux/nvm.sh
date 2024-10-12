51eb6a3b1717c6cc8a51dd483f8067b6c22c9dd7
Slaweekq <celesik@gmail.com>
2024-10-12 20:58:10 +0700

    1

--- Linux/nvm.sh
+++ Linux/nvm.sh
@@ -1,18 +1,18 @@
-echo 'Install nodejs & npm'
-echo '#################################################################'
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/nodesource.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt update
-sudo apt install -y nodejs
-export PATH="$PATH:$HOME/.npm/bin"
-source ~/.bashrc
-npm i -g npm@10.5.2 \
-  yarn prettier eslint solhint solidity-code-metrics nodemon serve \
-  dotenv nx nestjs nest-cli nats solc create-react-app npm-check-updates \
-  @nestjs/cli
+# echo 'Install nodejs & npm'
+# echo '#################################################################'
+# curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+# sudo apt update
+# sudo apt install -y nodejs
+# export PATH="$PATH:$HOME/.npm/bin"
+# source ~/.bashrc
+# npm i -g npm@10.5.2 \
+#   yarn prettier eslint solhint solidity-code-metrics nodemon serve \
+#   dotenv nx nestjs nest-cli nats solc create-react-app npm-check-updates \
+#   @nestjs/cli
 
 echo 'Install NVM & npm'
 echo '#################################################################'
+cd ~
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm --version
@@ -23,4 +23,8 @@ nvm use $VERSION
 nvm alias default $VERSION
 sudo chown -R "$USER":"$USER" ~/.npm
 sudo chown -R "$USER":"$USER" ~/.nvm
+npm i -g npm@10.5.2 \
+  yarn prettier eslint solhint solidity-code-metrics nodemon serve \
+  dotenv nx nestjs nest-cli nats solc create-react-app npm-check-updates \
+  @nestjs/cli
 clear && nvm ls
