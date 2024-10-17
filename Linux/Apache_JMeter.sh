eb23da29ba57da9bebf2c1e44dfa6c3c55d98b18
Slaweekq <celesik@gmail.com>
2024-10-17 19:07:57 +0700

    1

--- Linux/Apache_JMeter.sh
+++ Linux/Apache_JMeter.sh
@@ -1,15 +1,28 @@
+echo "Update package"
 sudo apt update -y
-sudo apt install -y openjdk-8-jdk apache2
+
+echo "Install java and apache"
+sudo apt install openjdk-11-jdk apache2 -y
+
+echo "Check version java"
 java -version
 
+echo "Start and enable apache2.service"
 sudo systemctl start apache2
 sudo systemctl enable apache2
 
+echo "Download apache-jmeter-5.6.3"
 wget https://downloads.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz
+
+echo "Extract apache-jmeter-5.6.3 to /opt/jmeter"
 sudo mkdir /opt/jmeter
 sudo tar -xvzf apache-jmeter-5.6.3.tgz -C /opt/jmeter --strip-components=1
 
+echo "Export path for /opt/jmeter/bin"
 tee -a ~/.bashrc <<< \
 '
 export PATH="$PATH:/opt/jmeter/bin"'
 source ~/.bashrc
+
+echo "Start UI: jmeter
+Start CLI: jmeter -n -t your_test_plan.jmx -l results.jtl"
