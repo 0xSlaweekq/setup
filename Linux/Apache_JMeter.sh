de674e585bd12ad82d1d37a58bb2f8084e70e9f6
Slaweekq <celesik@gmail.com>
2024-10-17 19:16:40 +0700

    1

--- Linux/Apache_JMeter.sh
+++ Linux/Apache_JMeter.sh
@@ -24,5 +24,27 @@ tee -a ~/.bashrc <<< \
 export PATH="$PATH:/opt/jmeter/bin"'
 source ~/.bashrc
 
-echo "Start UI: jmeter
+echo "Create desktop application"
+sudo tee -a /usr/share/applications/jmeter.desktop <<< \
+'
+[Desktop Entry]
+Name=JMeter
+Comment=Apache JMeter v5.6.3
+GenericName=JMeter Tests
+Exec=/opt/jmeter/bin %F
+Icon=jmeter
+Type=Application
+StartupNotify=false
+StartupWMClass=jmeter
+Categories=Network;
+Actions=new-window;
+Keywords=jmeter;
+
+[Desktop Action new-window]
+Name=New Window
+Name[ru]=Новое окно
+Exec=/opt/jmeter/bin --new-window %F
+Icon=jmeter'
+
+echo "Start UI: jmeter or alt+F2 -> JMeter in Applications
 Start CLI: jmeter -n -t your_test_plan.jmx -l results.jtl"
